c685476d375840fe69e088395d8bf4d92a8f12f3
Guilherme Costa <guilherme.partic@gmail.com>
2023-02-15 00:29:33 -0300

    feat: created the menu mobile

---
+++ base_static/global/js/script.js
@@ -0,0 +1,29 @@
+const menuMobile = document.querySelector('.c-header__nav_menu_mobile').addEventListener("click", controlMenu)
+
+function controlMenu() {
+    const navMenuUl = document.querySelector('.c-header__ul');
+    const navMenu = document.querySelector('.c-header__nav');
+    const iconMenu = document.querySelector('.c-header__nav_menu_mobile');
+
+    if (navMenuUl.classList.contains('is_closed')) {
+        navMenuUl.classList.remove('is_closed');
+        navMenuUl.classList.add('is_open');
+        navMenu.style.width = '150px';
+        iconMenu.style.right = '150px';
+        iconMenu.textContent = 'close';
+    } else {
+        navMenuUl.classList.remove('is_open');
+        navMenuUl.classList.add('is_closed');
+        navMenu.style.width = '5px';
+        iconMenu.style.right = '5px';
+        iconMenu.textContent = 'menu';
+    };
+};
+
+(function() {
+    const menuLink = document.querySelectorAll('.c-header__link');
+
+    for (const link of menuLink) {
+        link.addEventListener("click", controlMenu);
+    }
+})();
