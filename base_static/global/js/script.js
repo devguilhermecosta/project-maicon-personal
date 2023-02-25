a5bce27cd2721be33593c4870027da3843161906
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-02-25 19:27:26 -0300

    update: created the media queries for responsiveness

--- base_static/global/js/script.js
+++ base_static/global/js/script.js
@@ -1,17 +1,28 @@
+// menu and menu mobile
+const menuMobile = document.querySelector('.C-menu__span');
+menuMobile.addEventListener('click', () => {
+    const menuNav = document.querySelector('.C-menu__nav');
+
+    if (menuMobile.textContent == 'menu') {
+        menuMobile.textContent = 'close';
+        menuNav.classList.remove('is_hidden');
+    } else {
+        menuMobile.textContent = 'menu';
+        menuNav.classList.add('is_hidden');
+    };
+})
+
 // animate scroll
-
-const target = document.querySelectorAll('[data-anime]');
-const animationClass = 'animate';
-
-function animeScroll() {
-    const windowTop = window.scrollY + (window.innerHeight * 0.75);
+const animeScroll = ()=>{
+ 
+    const windowTop = window.scrollY + window.innerHeight * 0.75;
     
-    target.forEach(function(element) {
-        ((windowTop) > element.offsetTop) ?
-            element.classList.add(animationClass):element.classList.remove('animate');
-            console.log(element, element.offsetTop)
-    });
-}
-
-animeScroll();
+    const target = document.querySelectorAll('[data-anime]');
+    
+     target.forEach((element)=>{
+      (windowTop) > element.offsetTop
+      ? element.classList.add('animate') : element.classList.remove('animate')
+     })
+    }
+    
 window.addEventListener('scroll', animeScroll);
