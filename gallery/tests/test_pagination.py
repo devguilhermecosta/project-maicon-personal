ec2d747342c034f1bc16eac3d65d17a79d0e636e
Guilherme Costa <guilherme.partic@gmail.com>
2023-03-02 16:27:57 -0300

    feat: created the make_pagination

--- gallery/tests/test_pagination.py
+++ gallery/tests/test_pagination.py
@@ -2,6 +2,9 @@ import math
 
 from django.test import SimpleTestCase
 
+from django.http import HttpRequest
+from django.core.paginator import Paginator
+
 
 def make_pagination_range(range: list,
                           current_page: int,
@@ -22,7 +25,7 @@ def make_pagination_range(range: list,
         start_range = start_range - abs(stop_range - total_pages)
 
     return {
-        'pagination_range': range[start_range:stop_range],
+        'pagination': range[start_range:stop_range],
         'total_pages': total_pages,
         'range_per_page': range_per_page,
         'current_page': current_page,
@@ -31,6 +34,29 @@ def make_pagination_range(range: list,
         'first_page_out_of_range': current_page > middle_page,
         'last_page_out_of_range': stop_range < total_pages,
     }
+
+
+def make_pagination(request: HttpRequest,
+                    query_set: list[object],
+                    objects_per_page: int | str,
+                    range_per_page: int = 4,
+                    ) -> Paginator:
+
+    paginator: Paginator = Paginator(query_set,
+                                     objects_per_page,
+                                     )
+
+    try:
+        current_page: int = int(request.GET.get('page', 1))
+    except ValueError:
+        current_page = 1
+
+    page_object: Paginator = paginator.get_page(current_page)
+    paginator_range: dict = make_pagination_range(paginator.page_range,
+                                                  current_page,
+                                                  range_per_page
+                                                  )
+    return page_object, paginator_range
 
 
 class PaginationTest(SimpleTestCase):
@@ -43,57 +69,57 @@ class PaginationTest(SimpleTestCase):
                                            1,
                                            )
 
-        self.assertEqual([1, 2, 3, 4], pagination.get('pagination_range'))
+        self.assertEqual([1, 2, 3, 4], pagination.get('pagination'))
 
     def test_pagination_range_is_static_if_current_page_is_less_than_middle_page(self) -> None:  # noqa: E501
         pagintion_1: dict = make_pagination_range(self.range,
                                                   1,
                                                   4)
-        self.assertEqual([1, 2, 3, 4], pagintion_1.get('pagination_range'))
+        self.assertEqual([1, 2, 3, 4], pagintion_1.get('pagination'))
 
         pagination_2: dict = make_pagination_range(self.range,
                                                    2,
                                                    4)
-        self.assertEqual([1, 2, 3, 4], pagination_2.get('pagination_range'))
+        self.assertEqual([1, 2, 3, 4], pagination_2.get('pagination'))
 
         pagination_3: dict = make_pagination_range(self.range,
                                                    3,
                                                    4)
-        self.assertEqual([2, 3, 4, 5], pagination_3.get('pagination_range'))
+        self.assertEqual([2, 3, 4, 5], pagination_3.get('pagination'))
 
         pagination_4: dict = make_pagination_range(self.range,
                                                    4,
                                                    4)
 
-        self.assertEqual([3, 4, 5, 6], pagination_4.get('pagination_range'))
+        self.assertEqual([3, 4, 5, 6], pagination_4.get('pagination'))
 
     def test_pagination_range_shoud_be_static_at_end_of_range(self) -> None:
         pagination_1: dict = make_pagination_range(self.range,
                                                    18)
         self.assertEqual([17, 18, 19, 20],
-                         pagination_1.get('pagination_range')
+                         pagination_1.get('pagination')
                          )
 
         pagination_2: dict = make_pagination_range(self.range,
                                                    19)
         self.assertEqual([17, 18, 19, 20],
-                         pagination_2.get('pagination_range')
+                         pagination_2.get('pagination')
                          )
 
         pagination_3: dict = make_pagination_range(self.range,
                                                    20)
 
         self.assertEqual([17, 18, 19, 20],
-                         pagination_3.get('pagination_range')
+                         pagination_3.get('pagination')
                          )
 
     def test_pagination_is_correct_if_range_is_less_than_range_per_page(self) -> None:  # noqa: E501
         pagination_1: dict = make_pagination_range(list(range(1, 3)),
                                                    2)
 
-        self.assertEqual([1, 2], pagination_1.get('pagination_range'))
+        self.assertEqual([1, 2], pagination_1.get('pagination'))
 
         pagination_2: dict = make_pagination_range(list(range(1, 2)),
                                                    2)
 
-        self.assertEqual([1], pagination_2.get('pagination_range'))
+        self.assertEqual([1], pagination_2.get('pagination'))
