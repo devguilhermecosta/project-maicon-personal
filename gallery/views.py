488d998477c801a8fd7c835c3f1658eeab7f8e94
Guilherme Costa <guilherme.partic@gmail.com>
2023-03-02 16:44:13 -0300

    update: add pagination_range

--- gallery/views.py
+++ gallery/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 from django.views.generic.list import ListView
 from . models import Image
+from utils.paginator import make_pagination
 
 
 def gallery(request) -> render:
@@ -12,7 +13,6 @@ def gallery(request) -> render:
 class Gallery(ListView):
     model = Image
     template_name = 'pages/gallery.html'
-    paginate_by = 9
     ordering = '-id'
     context_object_name = 'gallery'
 
@@ -23,8 +23,14 @@ class Gallery(ListView):
     def get_context_data(self, *args, **kwargs):
         cd = super().get_context_data(*args, **kwargs)
 
+        page_object, pagination = make_pagination(self.request,
+                                                  cd.get('gallery'),
+                                                  9,  # criar uma variável de ambiente  # noqa: E501
+                                                  )
+
         cd.update({
-            'description': 'isso é mais um teste'
+            'images': page_object,
+            'pagination': pagination,
         })
 
         return cd
---
+++ utils/paginator/__init__.py
@@ -0,0 +1,3 @@
+# flake8: noqa
+
+from . pagination import make_pagination
