fb8b15606697c4ceaec672f3035ce7ac42223f16
Guilherme Costa <guilherme.partic@gmail.com>
2023-03-09 18:07:39 -0300

    feat: created the view for logout

--- author/urls.py
+++ author/urls.py
@@ -6,6 +6,7 @@ app_name: str = 'author'
 urlpatterns = [
     path('login/', views.login_view, name='login'),
     path('login/login_create/', views.login_create, name='login_create'),
+    path('logout/', views.LogoutView.as_view(), name='logout'),
     path('dashboard/', views.dashboard, name='dashboard'),
     path('dashboard/settings/socialnetwork/edit/',
          views.settings_socialnetwork,
--- author/views.py
+++ author/views.py
@@ -1,13 +1,15 @@
 from django.shortcuts import render, redirect
 from django.urls import reverse
 from django.http import HttpRequest, Http404
-from django.contrib.auth import login, authenticate
+from django.contrib.auth import login, logout, authenticate
 from django.contrib.auth.decorators import login_required
 from django.utils.decorators import method_decorator
 from django.contrib import messages
 from django.forms import ModelForm
 from django.views.generic import View
-from app_home.models import SocialNetwork, SectionIntro, Profile, PreGallery, Service, Adress
+from app_home.models import (
+    SocialNetwork, SectionIntro, Profile, PreGallery, Service, Adress, HomeContent
+    )
 from gallery.models import Image
 from utils.paginator import make_pagination
 from . forms import *
@@ -17,11 +19,14 @@ import os
 PER_PAGE_DASHBOARD: Optional[str] | str = os.environ.get('PER_PAGE_DASHBOARD')
 
 def login_view(request: HttpRequest) -> render:
+    home_content: HomeContent = HomeContent.objects.first()
+
     form: FormLogin = FormLogin()
 
     return render(request, 'author/pages/login.html', context={
         'form': form,
         'form_action': reverse('author:login_create'),
+        'home_content': home_content,
     })
 
 
@@ -43,18 +48,48 @@ def login_create(request: HttpRequest) -> render:
         else:
             messages.error(request, 'Usuário ou senha inválidos')
             return redirect('author:login')
-        
+
     return redirect('author:login')
+
+
+@method_decorator(
+    login_required(redirect_field_name='next',
+                   login_url='author:login',
+                   ),
+    'dispatch',
+)
+class LogoutView(View):
+
+    def post(self, request: HttpRequest) -> redirect:
+        username = request.user.username
+
+        if username is None:
+            messages.error(request, 'Invalid logout.')
+            return redirect(
+                reverse('author:login')
+                )
+
+        logout(request)
+
+        messages.success(request, 'Logout realizado com sucesso.')
+
+        return redirect(
+            reverse('author:login')
+        )
 
 
 @login_required(redirect_field_name='next', login_url='author:login')
 def dashboard(request: HttpRequest) -> render:
-  
-    return render(request, 'author/pages/dashboard.html',)
+    home_content: HomeContent = HomeContent.objects.first()
+
+    return render(request, 'author/pages/dashboard.html', context={
+        'home_content': home_content,
+    })
 
 @login_required(redirect_field_name='next', login_url='author:login')
 def settings_socialnetwork(request: HttpRequest):
     social: SocialNetwork = SocialNetwork.objects.first()
+    home_content: HomeContent = HomeContent.objects.first()
 
     form: ModelForm = SocialNetworkForm(data=request.POST or None,
                                    files=request.FILES or None,
@@ -70,12 +105,14 @@ def settings_socialnetwork(request: HttpRequest):
     return render(request, 'author/partials/_socialnetwork.html', context={
         'form': form,
         'button_to_back_action': reverse('author:dashboard'),
+        'home_content': home_content,
     })
 
 
 @login_required(redirect_field_name='next', login_url='author:login')
 def settings_sectionintro(request: HttpRequest):
     intro: SectionIntro = SectionIntro.objects.first()
+    home_content: HomeContent = HomeContent.objects.first()
 
     form: ModelForm = SectionIntroForm(data=request.POST or None,
                                        files=request.FILES or None,
@@ -91,12 +128,14 @@ def settings_sectionintro(request: HttpRequest):
     return render(request, 'author/partials/_sectionintro.html', context={
         'form': form,
         'button_to_back_action': reverse('author:dashboard'),
+        'home_content': home_content,
     })
 
 
 @login_required(redirect_field_name='next', login_url='author:login')
 def settings_profile(request: HttpRequest):
     profile: Profile = Profile.objects.first()
+    home_content: HomeContent = HomeContent.objects.first()
 
     form: ModelForm = ProfileForm(data=request.POST or None,
                                   files=request.FILES or None,
@@ -112,12 +151,14 @@ def settings_profile(request: HttpRequest):
     return render(request, 'author/partials/_profile.html', context={
         'form': form,
         'button_to_back_action': reverse('author:dashboard'),
+        'home_content': home_content,
     })
 
 
 @login_required(redirect_field_name='next', login_url='author:login')
 def settings_initial_gallery(request: HttpRequest):
     gallery: PreGallery = PreGallery.objects.first()
+    home_content: HomeContent = HomeContent.objects.first()
 
     form: ModelForm = PreGalleryForm(data=request.POST or None,
                                      files=request.FILES or None,
@@ -133,12 +174,14 @@ def settings_initial_gallery(request: HttpRequest):
     return render(request, 'author/partials/_initialgallery.html', context={
         'form': form,
         'button_to_back_action': reverse('author:dashboard'),
+        'home_content': home_content,
     })
 
 
 @login_required(redirect_field_name='next', login_url='author:login')
 def all_services(request: HttpRequest):
     services: Service = Service.objects.all()
+    home_content: HomeContent = HomeContent.objects.first()
 
     return render(request, 'author/partials/_all_services.html', context={
         'services': services,
@@ -146,6 +189,7 @@ def all_services(request: HttpRequest):
         'button_name': 'Novo Serviço',
         'button_action': reverse('author:new_service'),
         'aditional_class': 'C-service__delete',
+        'home_content': home_content,
     })
 
 
@@ -169,12 +213,15 @@ class ServiceView(View):
         return service
 
     def render_service(self, form: ServiceForm) -> render:
+        home_content: HomeContent = HomeContent.objects.first()
+        
         return render(
             self.request,
             'author/partials/_service.html',
             context={
                 'form': form,
                 'button_to_back_action': reverse('author:services'),
+                'home_content': home_content,
                 }
             )
 
@@ -231,8 +278,10 @@ class ServiceDeleteView(ServiceView):
         )
 
 
+@login_required(redirect_field_name='next', login_url='author:login')
 def settings_adress(request: HttpRequest):
     adress: Adress = Adress.objects.first()
+    home_content: HomeContent = HomeContent.objects.first()
 
     form: ModelForm = AdressForm(data=request.POST or None,
                                  files=request.FILES or None,
@@ -250,22 +299,33 @@ def settings_adress(request: HttpRequest):
     return render(request, 'author/partials/_adress.html', context={
         'form': form,
         'button_to_back_action': reverse('author:dashboard'),
+        'home_content': home_content,
     })
 
 
 @login_required(redirect_field_name='next', login_url='author:login')
 def gallery(request) -> render:
     form: ModelForm = GalleryForm()
+    home_content: HomeContent = HomeContent.objects.first()
 
     return render(request, 'author/partials/_gallery.html', context={
         'form': form,
         'button_to_back_action': reverse('author:dashboard'),
+        'home_content': home_content,
     })
 
 
+@method_decorator(
+    login_required(
+        redirect_field_name='next',
+        login_url='author:login'
+        ),
+    name='dispatch',
+)
 class GalleryAllImagesView(View):
     def get(self, request) -> render:
         images: Image = Image.objects.all().order_by('-id')
+        home_content: HomeContent = HomeContent.objects.first()
 
         page_object, pagination = make_pagination(request,
                                                   images,
@@ -278,10 +338,18 @@ class GalleryAllImagesView(View):
             'button_name': 'Nova Imagem',
             'button_action': reverse('author:new_image'),
             'button_to_back_action': reverse('author:dashboard'),
-            'aditional_class': 'C-image__delete'
+            'aditional_class': 'C-image__delete',
+            'home_content': home_content,
         })
 
 
+@method_decorator(
+    login_required(
+        redirect_field_name='next',
+        login_url='author:login'
+        ),
+    name='dispatch',
+)
 class GalleryImageView(View):
 
     def get_image(self, id=None) -> Image | None:
@@ -296,6 +364,7 @@ class GalleryImageView(View):
         return None
 
     def render_image(self, form) -> render:
+        home_content: HomeContent = HomeContent.objects.first()
 
         return render(
             self.request,
@@ -303,6 +372,7 @@ class GalleryImageView(View):
             context={
                 'form': form,
                 'button_to_back_action': reverse('author:gallery'),
+                'home_content': home_content,
                 },
             )
 
@@ -337,6 +407,13 @@ class GalleryImageView(View):
         return self.render_image(form)
 
 
+@method_decorator(
+    login_required(
+        redirect_field_name='next',
+        login_url='author:login'
+        ),
+    name='dispatch',
+)
 class GalleryDeleteView(GalleryImageView):
 
     def post(self, request, id=None):
