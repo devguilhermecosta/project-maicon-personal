e545b7d0f521fa4e6666a0d401bc27a200c7a92e
Guilherme Costa <guilherme.partic@gmail.com>
2023-03-15 20:22:56 -0300

    test: finished the address test

--- author/tests/tests_adress.py
+++ author/tests/tests_adress.py
@@ -1,12 +1,23 @@
-# flake8: noqa E501
-from django.test import TestCase
+# flake8: noqa
 from django.urls import reverse, resolve, ResolverMatch
 from django.http import HttpResponse
 from author.views import settings_adress
 from author.tests.author_base_test import AuthorTestBase
+from app_home.tests.home_base_test import make_adress
+from parameterized import parameterized
 
 
 class AdressTests(AuthorTestBase):
+    def setUp(self) -> None:
+        self.adress_data: dict = {
+            'name': 'this is the adress',
+            'adress': 'thi is the street',
+            'city': 'this is the city',
+            'postal': 'this is the postal',
+        }
+
+        return super().setUp()
+
     def test_settings_adress_url_is_correct(self) -> None:
         url: str = reverse('author:adress')
 
@@ -55,6 +66,7 @@ class AdressTests(AuthorTestBase):
                          )
 
     def test_settings_adress_url_is_different_when_user_is_redirected(self) -> None:
+        adress = make_adress()
         response: HttpResponse = self.client.get(
             reverse('author:adress')
         )
@@ -63,7 +75,63 @@ class AdressTests(AuthorTestBase):
                          '/author/login/?next=/author/dashboard/settings/adress/',
                          )
 
-        self.fail('Testar o preenchimento automático do formulário '
-                  'com o primeiro objeto do banco de dados. '
-                  'Testar o POST e as mensagens de aviso.'
-                  )
+
+    def test_settings_adress_form_used_first_object_created(self) -> None:
+        self.make_login()
+
+        adress_data: dict = {
+            'name': 'this is the adress',
+            'adress': 'thi is the street',
+            'city': 'this is the city',
+            'postal': 'this is the postal',
+        }
+        
+        make_adress(**adress_data)
+
+        response: HttpResponse = self.client.get(
+            reverse('author:adress')
+        )
+
+        response_content: str = response.content.decode('utf-8')
+
+        for key, value in adress_data.items():
+            self.assertIn(value, response_content)
+
+
+    @parameterized.expand([
+        ('name', 'Este campo é obrigatório'),
+        ('adress', 'Este campo é obrigatório'),
+        ('city', 'Este campo é obrigatório'),
+        ('postal', 'Este campo é obrigatório'),
+    ])
+    def test_settings_adress_form_used_fields_cannot_be_empty(self,
+                                                              field,
+                                                              message,
+                                                              ) -> None:
+        self.make_login()
+
+        self.adress_data[field] = ''
+
+        response: HttpResponse = self.client.post(
+            reverse('author:adress'),
+            data=self.adress_data,
+            follow=True,
+        )
+
+        response_content: str = response.content.decode('utf-8')
+
+        self.assertIn(message, response_content)
+        
+    def test_settings_adress_form_used_return_message_success_if_data_ok(self) -> None:
+        self.make_login()
+
+        response: HttpResponse = self.client.post(
+            reverse('author:adress'),
+            data=self.adress_data,
+            follow=True,
+        )
+
+        response_content: str = response.content.decode('utf-8')
+        message_succes: str = 'Dados salvos com sucesso'
+
+        self.assertIn(message_succes, response_content)
