59b8224ffee5f584e505513c726f5c20e75228cc
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-03-19 22:35:14 -0300

    test: initialized functional tests

--- author/tests/tests_gallery.py
+++ author/tests/tests_gallery.py
@@ -1,12 +1,18 @@
 from django.test import override_settings
+from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from django.urls import reverse, resolve, ResolverMatch
 from django.http import HttpResponse
 from . author_base_test import AuthorTestBase
 from author.views import GalleryImageView
 from gallery.models import Image
 from app_home.tests.home_base_test import make_simple_image
+from utils.browser import make_chrome_browser
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.common.by import By
+from django.contrib.auth.models import User
 import shutil
 import contextlib
+import pytest
 
 
 TEST_DIR = 'test_data'
@@ -83,3 +89,51 @@ class GallerySettingsTests(AuthorTestBase):
             '1 - criar um teste normal para editar e deletar images.\n'
             '2 - criar um test funcional.'
         )
+
+
+@pytest.mark.functional_test
+class GallerySettingsFunctionalTests(StaticLiveServerTestCase):
+    def setUp(self) -> None:
+        self.browser: WebDriver = make_chrome_browser()
+        return super().setUp()
+
+    def tearDown(self) -> None:
+        self.browser.quit()
+        return super().tearDown()
+
+    def test_gallery_edit_image(self) -> None:
+        from time import sleep
+
+        user_data: dict = {
+            'username': 'username',
+            'password': 'password',
+        }
+
+        User.objects.create_user(**user_data)
+
+        self.browser.get(
+            self.live_server_url +
+            reverse('author:login')
+            )
+
+        input_user = self.browser.find_element(
+            By.XPATH,
+            '//*[@id="id_username"]',
+        )
+        input_user.send_keys(user_data['username'])
+
+        input_password = self.browser.find_element(
+            By.XPATH,
+            '//*[@id="id_password"]',
+        )
+        input_password.send_keys(user_data['password'])
+
+        button_submit = self.browser.find_element(
+            By.XPATH,
+            '/html/body/main/section/form/button'
+        )
+        button_submit.click()
+
+        sleep(10)
+
+        self.fail('continuer from here')
---
+++ utils/browser.py
@@ -0,0 +1,54 @@
+from selenium import webdriver
+from selenium.webdriver import ChromeOptions
+from selenium.webdriver import Chrome
+from selenium.webdriver.chrome.webdriver import WebDriver
+from selenium.webdriver.chrome.service import Service
+from pathlib import Path
+import sys
+import os
+
+
+ROOT_PATH = Path(__file__).parent.parent
+
+WEBDRIVER_NAME: dict = {
+    'windows': 'chromedriver_windows.exe',
+    'ubuntu': 'chromedriver_ubuntu.exe',
+}
+SYSTEM = sys.platform
+
+if SYSTEM == 'win32':
+    CHROMEDRIVER_PATH = str(
+        ROOT_PATH / 'bin' / WEBDRIVER_NAME['windows']
+    )
+elif SYSTEM == 'linux':
+    CHROMEDRIVER_PATH = str(
+        ROOT_PATH / 'bin' / WEBDRIVER_NAME['ubuntu']
+    )
+
+
+def make_chrome_browser(*options) -> WebDriver:
+    chrome_options: ChromeOptions = ChromeOptions()
+
+    for option in options:
+        chrome_options.add_argument(option)
+
+    if os.environ.get('SELENIUM_HEADLESS') == '0':
+        chrome_options.add_argument('--headless')
+
+    chrome_service: Service = Service(executable_path=CHROMEDRIVER_PATH)
+
+    browser: Chrome = webdriver.Chrome(
+        service=chrome_service,
+        options=chrome_options,
+        )
+
+    return browser
+
+
+if __name__ == '__main__':
+    from time import sleep
+
+    browser = make_chrome_browser()
+    browser.get('https://www.google.com')
+    sleep(2)
+    browser.quit()
