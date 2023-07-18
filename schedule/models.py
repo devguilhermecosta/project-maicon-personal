b80dda26d7258f7712fd9fac9e436238c870a28b
Guilherme Costa <guilherme.partic@gmail.com>
2023-07-18 18:17:00 -0300

    feat: created the Appointmente model

--- schedule/models.py
+++ schedule/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Appointment(models.Model):
+    category = models.CharField(max_length=50, choices=(
+        ('eletro', 'eletro'),
+        ('musculação', 'musculação'),
+    ))
+    date = models.DateTimeField()
+    client_name = models.CharField(max_length=255)
+    client_phone = models.CharField(max_length=255)
+    description = models.TextField()
+
+    def __str__(self) -> str:
+        return f'{self.category} - {self.date} - {self.client_name}'
