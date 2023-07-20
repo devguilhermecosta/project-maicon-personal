8e09b122307d2f656544356be47aeebf9771ca14
Guilherme Costa <98500282+devguilhermecosta@users.noreply.github.com>
2023-07-19 22:46:19 -0300

    update: add the feedback field into model

--- schedule/models.py
+++ schedule/models.py
@@ -10,6 +10,7 @@ class Appointment(models.Model):
     client_name = models.CharField(max_length=255)
     client_phone = models.CharField(max_length=255)
     description = models.TextField()
+    feedback = models.BooleanField(default=False, blank=False, null=False)
 
     def __str__(self) -> str:
         return f'{self.category} - {self.date} - {self.client_name}'
